36fe91debc478a805d5976208572e2203fd518bb
Ib Helmer Nielsen <ihn@ihn.dk>
2025-02-06 20:06:56 +0100

    Modul2

---
+++ Modul2/exer06.py
@@ -0,0 +1,6 @@
+def main():
+    coordinats = (4.5,6.0)
+    print(coordinats[0])
+    coordinats[1]=10.0
+if __name__ == "__main__":
+    main()
---
+++ Modul2/exer07.py
@@ -0,0 +1,10 @@
+def main():
+    set_a = {1, 2, 3, 4}
+    set_b = {3, 4, 5, 6}
+    print(set_a.union(set_b))
+    print(set_a.intersection(set_b))
+
+if __name__ == "__main__":
+    main()
+
+
